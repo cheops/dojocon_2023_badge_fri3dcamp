71b8e7855b31271812f2a6698b010f5a75a5a17c
Joram Agten <joram.agten@vito.be>
2023-01-27 21:49:26 +0100

    printing output

--- micropython_example_2_tft/example2_tft.py
+++ micropython_example_2_tft/example2_tft.py
@@ -28,20 +28,22 @@ def init_screen():
 def logo_coderdojo():
     import logo_coderdojo
     screen.bitmap(logo_coderdojo, 0, 0)
-    print("drawing coderdojo logo")
+    print("drawing logo coderdojo")
 
 def logo_coderdojo_smooth():
     import logo_coderdojo_smooth
     screen.bitmap(logo_coderdojo_smooth, 0, 0)
-    print("drawing coderdojo logo smooth ")
+    print("drawing logo coderdojo smooth")
 
 def logo_fri3d():
     import logo_fri3d
     screen.bitmap(logo_fri3d, 0, 0)
+    print("drawing logo fri3d")
 
 def logo_fri3d_green_circles():
     import logo_fri3d_green_circles
     screen.bitmap(logo_fri3d_green_circles, 0, 0)
+    print("drawing logo fri3d green circles")
 
 def main():
     print("running main")
